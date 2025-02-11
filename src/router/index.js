e14f3edc7d49aa2964b7a462f0d37c480dbd8cc2
wss <wangsaisai@baiwang.com>
2025-02-11 19:48:33 +0800

    feat: 修改动态导入路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,96 +6,96 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: import('@/page/home/index.vue'),
+            component: () => import('@/page/home/index.vue'),
         },
         // 名言广场
         {
             path: '/famousSquare',
             name: 'famousSquare',
-            component: import('@/page/famousSquare/index.vue'),
+            component: () => import('@/page/famousSquare/index.vue'),
         },
         // 工具箱
         {
             path: '/toolbox',
             name: 'toolbox',
-            component: import('@/page/toolbox/index.vue'),
+            component: () => import('@/page/toolbox/index.vue'),
         },
         // 标签
         {
             path: '/label',
             name: 'label',
-            component: import('@/page/label/index.vue'),
+            component: () => import('@/page/label/index.vue'),
         },
         // 关于
         {
             path: '/about',
             name: 'about',
-            component: import('@/page/about/index.vue'),
+            component: () => import('@/page/about/index.vue'),
         },
         // 友链
         {
             path: '/friendChain',
             name: 'friendChain',
-            component: import('@/page/friendChain/index.vue'),
+            component: () => import('@/page/friendChain/index.vue'),
         },
         // 关注
         {
             path: '/attention-article',
             name: 'attention-article',
-            component: import('@/page/attentionArticle/index.vue'),
+            component: () => import('@/page/attentionArticle/index.vue'),
         },
         // 最新
         {
             path: '/last-article',
             name: 'last-article',
-            component: import('@/page/lastArticle/index.vue'),
+            component: () => import('@/page/lastArticle/index.vue'),
         },
         // 前端
         {
             path: '/frontend',
             name: 'frontend',
-            component: import('@/page/frontend/index.vue'),
+            component: () => import('@/page/frontend/index.vue'),
         },
         // 后端
         {
             path: '/backend',
             name: 'backend',
-            component: import('@/page/backend/index.vue'),
+            component: () => import('@/page/backend/index.vue'),
         },
         // Android
         {
             path: '/android',
             name: 'android',
-            component: import('@/page/android/index.vue'),
+            component: () => import('@/page/android/index.vue'),
         },
         // iOS
         {
             path: '/ios',
             name: 'ios',
-            component: import('@/page/ios/index.vue'),
+            component: () => import('@/page/ios/index.vue'),
         },
         // 人工智能
         {
             path: '/ai',
             name: 'ai',
-            component: import('@/page/ai/index.vue'),
+            component: () => import('@/page/ai/index.vue'),
         },
         // 开发工具
         {
             path: '/freebie',
             name: 'freebie',
-            component: import('@/page/freebie/index.vue'),
+            component: () => import('@/page/freebie/index.vue'),
         },
         // 操作系统
         {
             path: '/operatingSystem',
             name: 'operatingSystem',
-            component: import('@/page/operatingSystem/index.vue'),
+            component: () => import('@/page/operatingSystem/index.vue'),
         },
         {
             path: '/editor',
             name: 'editor',
-            component: import('@/page/aieditor/index.vue'),
+            component: () => import('@/page/aieditor/index.vue'),
         },
     ],
 })
