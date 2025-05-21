e100cc37f40f409e3f2ef4c7652dcb874197b7f2
wss <wangsaisai@baiwang.com>
2025-05-21 15:35:19 +0800

    feat: 测试jenkins

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import { getLocalStorage } from './utils'
 import { debounce } from 'lodash-es'
 import { getUnreadNotificationsRequest } from './api/module/notification'
 
+console.log('测试jenkins')
+
 const store = appStore()
 // setLocalStorage('userInfo', {
 //     cookie: 'qqqq',
