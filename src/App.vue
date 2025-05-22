85e92457e9923bb3e89582eb44e6a0590463ba33
wss <wangsaisai@baiwang.com>
2025-05-22 09:37:45 +0800

    feat: 测试jenkins9

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ console.log('测试jenkins5')
 console.log('测试jenkins6')
 console.log('测试jenkins7')
 console.log('测试jenkins8')
+console.log('测试jenkins9')
 
 const store = appStore()
 // setLocalStorage('userInfo', {
